0cf72e579c707845c00f92a115ba8ea3a823a0be
osama11osama <a.d.g.osama@gmail.com>
2025-10-26 19:14:41 +0100

    feat(1016A): Death_Note — cerated

---
+++ 2025/10-26/1016A_Death_Note/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import sys
+
+
+def solve():
+    data = sys.stdin.read().strip().split()  # noqa: F841
+    # TODO: implement using data
+    # print(answer)
+
+
+if __name__ == "__main__":
+    solve()
