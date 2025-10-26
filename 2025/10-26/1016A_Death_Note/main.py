cd3447451cd4d46b857017361f41b5a2f9764c9f
osama11osama <a.d.g.osama@gmail.com>
2025-10-27 00:23:17 +0100

    feat(1016A): Death_Note — solved

--- 2025/10-26/1016A_Death_Note/main.py
+++ 2025/10-26/1016A_Death_Note/main.py
@@ -3,9 +3,28 @@ import sys
 
 
 def solve():
-    data = sys.stdin.read().strip().split()  # noqa: F841
-    # TODO: implement using data
-    # print(answer)
+    data = list(map(int, sys.stdin.read().strip().split()))
+    if not data:
+        print(0)
+        return
+    else:
+        m = data[1]
+        a = data[2:]
+
+        pref = 0        # running sum of a
+        k = 0  # running sum of t
+        t_list = []
+
+        for x in a:
+            pref += x
+            # t = int(sum(a_lsit[:i+1])/m) - k
+            t = pref // m - k
+            t_list.append(t)
+            k+=t
+            
+        # print("n =", n, " m =", m)
+        print(*t_list)
+        
 
 
 if __name__ == "__main__":
